e19c825913b3cc55a8e3c2bd277f990de6231b22
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-01-15 18:18:55 -0500

    Time: 4732 ms (69.39%), Space: 62.8 MB (8.50%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.py
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.py
@@ -1,22 +1,22 @@
 class Solution:
     def minimizeTheDifference(self, mat: List[List[int]], target: int) -> int:
-        m,n = len(mat),len(mat[0])
+        m, n = len(mat), len(mat[0])
+        
+        
         
         for i in range(m):
             mat[i] = sorted(set(mat[i]))
         
         @lru_cache(None)
         def helper(r,s):
-            if r == -1:
-                return abs(target-s)
-            
-            ans = float('inf')
-            for j in mat[r]:
-                ans = min(ans,helper(r-1,s+j))
-                if s + j >= target:
-                    break
+            if r == m:
+                return abs(s - target)
 
+            ans = float('inf')
+            for i in mat[r]:
+                ans= min(ans,helper(r+1,s+i))
+                if s + i >= target:
+                    break                       
             return ans
         
-        return helper(m-1,0)
-        
+        return helper(0,0)
