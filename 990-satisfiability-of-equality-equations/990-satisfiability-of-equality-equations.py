9a8eb891b401d272d38e7caa51673b4f8ca1954f
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-01-21 03:02:31 -0500

    Time: 62 ms (35.28%), Space: 14.6 MB (29.04%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -2,7 +2,7 @@ class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
         adj = defaultdict(list)
         
-        
+        nq = []
         def con(s):
             return ord(s)-ord('a')
         
@@ -14,6 +14,8 @@ class Solution:
                 b = con(eq[-1])
                 adj[a].append(b)
                 adj[b].append(a)
+            else:
+                nq.append(eq)
 
         #2 assign values to components
         val = [None for i in range(26)]
@@ -32,11 +34,11 @@ class Solution:
             
 
         #3 check 
-        for eq in equations:
-            if eq[1]=='!':
-                a = con(eq[0])
-                b = con(eq[-1])
-                if val[a] == val[b]:
-                    return False
+        for eq in nq:
+
+            a = con(eq[0])
+            b = con(eq[-1])
+            if val[a] == val[b]:
+                return False
                 
         return True
