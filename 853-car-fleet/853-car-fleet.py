cb251233856f4a8ead0660f82645e42287b08a8d
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-01-21 03:57:27 -0500

    Time: 1275 ms (30.51%), Space: 40.1 MB (7.67%) - LeetHub

--- 853-car-fleet/853-car-fleet.py
+++ 853-car-fleet/853-car-fleet.py
@@ -2,7 +2,6 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         t = [[(target-i[0]), (target-i[0])/i[1]] for i in zip(position,speed)]
         t.sort(reverse=True)
-        print(t)
         ans = 1
         for i in range(len(speed)-2,-1,-1):
             if t[i][1] <= t[i+1][1]:
