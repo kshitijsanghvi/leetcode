9c44b5b656dff55c1aad150b2eeb434e483a7d3c
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-01-24 00:27:24 -0500

    Time: 1301 ms (12.34%), Space: 19.6 MB (65.45%) - LeetHub

---
+++ 886-possible-bipartition/886-possible-bipartition.py
@@ -0,0 +1,63 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        d = [sorted(i) for i in dislikes]
+        d.sort()
+        
+        d1 = {}
+        d2 = {}
+        
+        prev = 0
+        
+        while d:
+            ud = []
+            if prev == len(d):
+                i = d[0]
+                if i[0] in d1:
+                    d2[i[1]] = 1
+
+
+                elif i[0] in d2:
+                    d1[i[1]] = 1
+
+
+                elif i[1] in d1:
+                    d2[i[0]] = 1
+
+
+                elif i[1] in d2:
+                    d1[i[0]] = 1
+                else:
+                    d1[i[0]] = 1
+                    d2[i[1]] = 1
+
+                d= d[1:]
+
+            for i in d:
+                if i[0] in d1 and i[1] in d1:
+                    return False
+
+                if i[0] in d2 and i[1] in d2:
+                    return False
+
+                if i[0] in d1:
+                    d2[i[1]] = 1
+                    continue
+
+                if i[0] in d2:
+                    d1[i[1]] = 1
+                    continue
+
+                if i[1] in d1:
+                    d2[i[0]] = 1
+                    continue
+
+                if i[1] in d2:
+                    d1[i[0]] = 1
+                    continue
+                
+                ud.append(i)
+            
+            prev = len(d)
+            d = ud
+            
+        return True
