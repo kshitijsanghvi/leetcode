0a479fd4be65f9fbff22232f42ecb1f337df8c02
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-01-24 02:48:17 -0500

    Time: 2091 ms (29.20%), Space: 46.9 MB (46.71%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        adj = defaultdict(list)
+        root = None
+        
+        for i,v in enumerate(manager):
+            if v == -1:
+                root = i
+            adj[v].append(i)
+            
+        time = [0 for i in range(n)]
+        q = [root]
+        while q:
+            cn = q.pop(0)
+            
+            for nn in adj[cn]:
+                
+                time[nn] = time[cn] + informTime[cn]
+                q.append(nn)
+        return max(time)
