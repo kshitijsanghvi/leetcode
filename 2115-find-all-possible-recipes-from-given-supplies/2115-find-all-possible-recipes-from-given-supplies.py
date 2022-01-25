fadc6e0cd22632693f675f8eabb01b204bfc8748
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-01-24 20:21:00 -0500

    Time: 1742 ms (30.77%), Space: 17.2 MB (23.83%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,60 @@
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        dr = {}
+        ds = {}
+        n = len(recipes)
+        adj = defaultdict(list)
+        
+        c = 0
+        for r in recipes:
+            dr[r] = c
+            c += 1
+        for s in supplies:
+            ds[s] = 1
+            
+        for i ,v in enumerate(ingredients):
+            for j in v:
+                if j in dr:
+                    adj[dr[j]].append(i)
+        
+        
+        def dfs(cn):
+            nonlocal v
+            nonlocal adj
+            nonlocal ca
+            v[cn] = 1
+            for nn in adj[cn]:
+                if nn not in v:
+                    dfs(nn)
+            ca.append(cn)
+        
+        ans = []
+        v = {}
+        for i in range(n):
+            if i not in v:
+                ca = []
+                dfs(i)
+                ans += ca
+                
+        ans = ans[::-1]
+        canmake = defaultdict(bool)
+        
+        for i in ans:
+            flag = True
+            for j in ingredients[i]:
+                if j in dr:
+                    if canmake[dr[j]] == False:
+                        canmake[i] = False
+                        flag = False
+                        break
+                else:
+                    if j not in ds:
+                        canmake[i] = False
+                        flag = False
+                        break
+            if flag:
+                canmake[i] = True
+        
+        return [v for i,v in enumerate(recipes) if canmake[i] ==  True]
+            
+        
