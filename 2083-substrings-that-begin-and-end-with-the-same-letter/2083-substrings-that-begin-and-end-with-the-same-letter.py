e8d7298913ec8f61d50612df3672436c80f4aedd
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-01-27 16:12:01 -0500

    Time: 65 ms (77.30%), Space: 15.2 MB (38.88%) - LeetHub

--- 2083-substrings-that-begin-and-end-with-the-same-letter/2083-substrings-that-begin-and-end-with-the-same-letter.py
+++ 2083-substrings-that-begin-and-end-with-the-same-letter/2083-substrings-that-begin-and-end-with-the-same-letter.py
@@ -1,4 +1,3 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        c = Counter(s)
-        return sum([v*(v+1)//2 for v in c.values()])
+        return sum([v*(v+1)//2 for v in Counter(s).values()])
