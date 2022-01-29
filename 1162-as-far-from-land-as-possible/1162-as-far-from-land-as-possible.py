a10967be67a08e071e3648c9da75b6f53e8b5487
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-01-29 02:07:42 -0500

    Time: 839 ms (41.33%), Space: 14.7 MB (98.64%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -2,14 +2,14 @@ class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
         m , n = len(grid),len(grid[0])
         
-        q = []
+        q = deque([])
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     q.append([i,j])
         ans = 0
         while q:
-            [cx,cy] = q.pop(0)
+            [cx,cy] = q.popleft()
             
             l = (cx,cy-1)
             r = (cx,cy+1)
