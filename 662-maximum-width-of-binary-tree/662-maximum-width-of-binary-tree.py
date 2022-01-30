24286032be95dba79b9f499992fc481660c4db2a
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-01-29 20:22:17 -0500

    Time: 83 ms (13.07%), Space: 20.2 MB (5.11%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.py
@@ -7,19 +7,22 @@
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         d = {}
+        max_v = float('-inf')
         def helper(node,idx,current_depth):
             nonlocal d
+            nonlocal max_v
             if not node:
                 return
             l = 2 *idx + 1
             r = 2 *idx + 2
             next_depth = current_depth + 1
             if current_depth not in d:
-                d[current_depth] = [idx,idx,1]
+                d[current_depth] = [idx,idx]
+                max_v = max(max_v,1)
             else:
                 d[current_depth][0] = min(d[current_depth][0],idx)
                 d[current_depth][1] = max(d[current_depth][1],idx)
-                d[current_depth][2] = d[current_depth][1] - d[current_depth][0] + 1
+                max_v = max(max_v,d[current_depth][1]-d[current_depth][0]+1)
             
             if node.left:
                 helper(node.left,l,next_depth)
@@ -28,7 +31,7 @@ class Solution:
             
             return 
         helper(root,0,0)
-        return max([d[i][2] for i in d])
+        return max_v
                 
             
             
