806c0549746cec88e105f65d9f6432cd80b2c695
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-01-31 12:31:25 -0500

    Time: 8075 ms (5.04%), Space: 15.5 MB (78.10%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -2,25 +2,45 @@ class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         m = len(heights)
         n = len(heights[0])
-        v = {}
-        h = []
-        heapq.heappush(h,[0,0,0])
-        v[0,0] = 0
-        while h:
-            cd,cx,cy = heapq.heappop(h)
-            if cx ==  m-1 and cy == n - 1:
-                return cd
+        
+        l = 0
+        r = 10**6
+        
+        
+        def check(limit):
+            nonlocal m,n
+            q = deque()
+            q.append([0,0,0])
+            v = {}
+            v[0,0] = 0
+            while q:
+                cd,cx,cy = q.popleft()
+                if cx == m-1 and cy == n-1:
+                    return True
+                else:
+                    
+                    l = (cx,cy-1)
+                    r = (cx,cy+1)
+                    t = (cx-1,cy)
+                    b = (cx+1,cy)
+                    
+                    for nx,ny in [l,r,t,b]:
+                        if 0<= nx< m and 0<=ny<n:
+                            key = max(cd,abs(heights[nx][ny]-heights[cx][cy]))
+                            if key <= limit:
+                                if (nx,ny) not in v:
+                                    v[nx,ny] = key
+                                    q.append([key,nx,ny])
+            return False
+        
+        while l + 1 < r:
+            mid = l + (r-l)//2
+            if check(mid):
+                r = mid
             else:
+                l = mid + 1
                 
-                l = (cx,cy-1)
-                r = (cx,cy+1)
-                t = (cx-1,cy)
-                b = (cx+1,cy)
-                
-                for nx,ny in [l,r,t,b]:
-                    if 0<= nx < m and 0 <= ny < n:
-                        key = max(cd,abs(heights[nx][ny]-heights[cx][cy]))
-                        if (nx,ny) not in v or v[nx,ny] > key:
-                            v[nx,ny] = key
-                            heapq.heappush(h,[key,nx,ny])
-        return 0
+        if check(l):
+            return l
+        else:
+            return r 
