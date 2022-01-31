0ddd24695fc9eca468f69b7b272a454a22bed623
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-01-30 20:33:35 -0500

    Time: 770 ms (17.60%), Space: 21.2 MB (52.24%) - LeetHub

---
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.py
@@ -0,0 +1,32 @@
+class Solution:
+    def gardenNoAdj(self, n: int, paths: List[List[int]]) -> List[int]:
+        color = [None for _ in range(n+1)]
+        
+        if n <= 4:
+            return [i+1 for i in range(n)]
+        
+        adj = defaultdict(list)
+        for i in paths:
+            adj[i[0]].append(i[1])
+            adj[i[1]].append(i[0])
+            
+        for i in range(1,n+1):
+            if color[i] == None:
+                color[i] = 1
+                
+                q = deque()
+                for nn in adj[i]:
+                    q.append(nn)
+                
+                while q:
+                    cn = q.popleft()
+                    if color[cn] == None:
+                        l = [1,2,3,4]
+                        for nn in adj[cn]:
+                            if color[nn] != None and color[nn] in l:
+                                l.remove(color[nn])
+                            else:
+                                q.append(nn)
+                        color[cn] = l[0]
+        
+        return color[1:]
