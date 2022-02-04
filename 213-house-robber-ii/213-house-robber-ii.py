3832417638ec5130e9a937f04053e972764474ac
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-04 16:58:54 -0500

    Time: 74 ms (5.29%), Space: 14 MB (82.18%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -3,21 +3,24 @@ class Solution:
         # case 1
         if len(nums) == 1:
             return nums[0]
-        @lru_cache(None)
+        dp = {}
         def helper(i):
             if i >= len(nums) - 1:
                 return 0
-          
-            return max(nums[i] + helper(i+2), helper(i+1))
+            if i not in dp:
+                dp[i] = max(nums[i] + helper(i+2), helper(i+1))
             
+            return dp[i]
         
         a = helper(0)
-        @lru_cache(None)
+        
+        dp = {}
         def helper1(i):
             if i >= len(nums):
                 return 0
-            return max(nums[i] + helper1(i+2), helper1(i+1))
+            if i not in dp:
+                dp[i] = max(nums[i] + helper1(i+2), helper1(i+1))
             
-             
+            return dp[i]
         b = helper1(1)
         return max(a,b)
