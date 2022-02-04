56fe0a065e1fa33c271e1a6c7a58ef81e24264be
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-04 01:20:04 -0500

    Time: 991 ms (5.04%), Space: 22.6 MB (64.48%) - LeetHub

--- 582-kill-process/582-kill-process.py
+++ 582-kill-process/582-kill-process.py
@@ -1,27 +1,20 @@
 class Solution:
     def killProcess(self, pid: List[int], ppid: List[int], kill: int) -> List[int]:
-        
+
         p = {}
-        child = defaultdict(list)
-        
         for i,v in enumerate(ppid):
             p[pid[i]] = v
-            child[v].append(pid[i])
-        
+            
         if p[kill] == 0:
             return pid
-        
-        v = {}
-        
-        v[kill] = 1
-        q = [kill]
         ans = []
-        while q:
-            cn = q.pop()
-            ans.append(cn)
-            for nn in child[cn]:
-                if nn not in v:
-                    v[nn] = 1
-                    q.append(nn)
-                    
+        p[kill]=kill
+        for i in pid:
+            copy = i
+            while p[i]!=kill and p[i]!=0:
+                i = p[i]
+                
+            if p[i] == kill:
+                ans.append(copy)
+                
         return ans
