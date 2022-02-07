1396013f603ff26d459a613cb333100b8b3ca53c
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-07 11:04:24 -0500

    Time: 96 ms (88.73%), Space: 16.7 MB (51.57%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,51 +1,37 @@
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        from collections import defaultdict
-        courseDict = defaultdict(list)
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        '''
+        0 -> 1 <- 2
+          <-
+        '''
 
-        for relation in prerequisites:
-            nextCourse, prevCourse = relation[0], relation[1]
-            courseDict[prevCourse].append(nextCourse)
-
-        checked = [False] * numCourses
-        path = [False] * numCourses
-
-        for currCourse in range(numCourses):
-            if self.isCyclic(currCourse, courseDict, checked, path):
+        adj = [[] for i in range(numCourses)]
+        for a, b in prerequisites:
+            if a == b:
                 return False
-        return True
+            adj[b].append(a)
 
+        
+        v = [0 for i in range(numCourses)]
 
-    def isCyclic(self, currCourse, courseDict, checked, path):
-        """   """
-        # 1). bottom-cases
-        if checked[currCourse]:
-            # this node has been checked, no cycle would be formed with this node.
-            return False
-        if path[currCourse]:
-            # came across a marked node in the path, cyclic !
+        def dfs(cn,adj,v):
+            for nn in adj[cn]:
+                if v[nn] == 1:
+                    return False
+                elif v[nn]==0:
+                    v[nn] = 1
+                    res = dfs(nn,adj,v)
+                    if not res:
+                        return False
+            v[cn] = 2
             return True
 
-        # 2). postorder DFS on the children nodes
-        # mark the node in the path
-        path[currCourse] = True
+        for r in range(numCourses):
+            if v[r]==0:
+                v[r] = 1
+                res = dfs(r,adj,v)
+                if not res:
+                    return False
+                v[r] = 2
 
-        ret = False
-        # postorder DFS, to visit all its children first.
-        for child in courseDict[currCourse]:
-            ret = self.isCyclic(child, courseDict, checked, path)
-            if ret: break
-
-        # 3). after the visits of children, we come back to process the node itself
-        # remove the node from the path
-        path[currCourse] = False
-
-        # Now that we've visited the nodes in the downstream,
-        #   we complete the check of this node.
-        checked[currCourse] = True
-        return ret
+        return True 
