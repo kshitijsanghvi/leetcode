4f475bad48055902fcc347844b209ec0a6baaf22
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-07 14:11:46 -0500

    Time: 7017 ms (21.50%), Space: 44.5 MB (5.03%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        dp = {}
-        n = len(nums)
         
+        n = len(nums)
+        @lru_cache(None)
         def helper(i):
             if i >= n-1:
                 return 0
             
-            if i not in dp:
-                dp[i] = 1 + min([float('inf')] + [helper(i+j) for j in range(1,nums[i]+1)])
+
+            return 1 + min([float('inf')] + [helper(i+j) for j in range(1,nums[i]+1)])
             
-            return dp[i]
-        
         return helper(0)
