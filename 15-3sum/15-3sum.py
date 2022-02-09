b0449f4d21a2dc94e36f022b760c72d1c7a8f0ab
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-08 19:27:24 -0500

    Time: 3659 ms (21.67%), Space: 19.4 MB (5.18%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,13 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n= len(nums)
-        ans = []
+        nums.sort()
+        ans =set()
+        res = []
         for i in range(n):
             find = -nums[i]
             d = {}
             for j in range(i+1,n):
                 if find - nums[j] in d:
-                    ans.append(tuple(sorted([nums[i],nums[d[find - nums[j]]],nums[j]])))
+                    a = (nums[i],nums[d[find - nums[j]]],nums[j])
+                    if a not in ans:
+                        res.append(list(a))
+                        ans.add(a)
                 
                 d[nums[j]] = j
-        return list(set(ans))
+        return res
