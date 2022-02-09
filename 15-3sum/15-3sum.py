c04675283f12b886c71fe1d0fbd80dcf2310bfc4
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-08 19:30:50 -0500

    Time: 954 ms (70.30%), Space: 19.2 MB (6.05%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -5,14 +5,15 @@ class Solution:
         ans =set()
         res = []
         for i in range(n):
-            find = -nums[i]
-            d = {}
-            for j in range(i+1,n):
-                if find - nums[j] in d:
-                    a = (nums[i],nums[d[find - nums[j]]],nums[j])
-                    if a not in ans:
-                        res.append(list(a))
-                        ans.add(a)
-                
-                d[nums[j]] = j
+            if i == 0 or  nums[i]!= nums[i-1]:
+                find = -nums[i]
+                d = {}
+                for j in range(i+1,n):
+                    if find - nums[j] in d:
+                        a = (nums[i],nums[d[find - nums[j]]],nums[j])
+                        if a not in ans:
+                            res.append(list(a))
+                            ans.add(a)
+
+                    d[nums[j]] = j
         return res
