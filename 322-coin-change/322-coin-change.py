7c5bb2c5b72a98f02a716bf49e56bb8d7343ee85
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-10 15:11:22 -0500

    Time: 1532 ms (67.87%), Space: 14.2 MB (84.80%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,18 +1,11 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = {}
         
-        def helper(amount):
-            if amount == 0:
-                return 0
-            if amount < 0:
-                return float('inf')
-            
-            
-            if amount not in dp:
-                dp[amount] = 1 + min([helper(amount - c) for c in coins])
-                
-            return dp[amount]
+        dp = [float('inf') for i in range(amount+1)]
+        dp[0] = 0
         
-        v = helper(amount)
-        return v if v != float('inf') else -1
+        for i in range(1,amount+1):
+            for c in coins:
+                if c <= i:
+                    dp[i] = min(dp[i], 1 + dp[i-c])
+        return dp[-1] if dp[-1] != math.inf else -1
