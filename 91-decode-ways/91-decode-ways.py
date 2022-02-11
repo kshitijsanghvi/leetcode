fcc35d54972ace99e3b579f65dd65adbc3e1106e
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-10 20:11:42 -0500

    Time: 76 ms (5.25%), Space: 13.9 MB (95.57%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,22 +1,12 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         d = {str(i) : 1 for i in range(1,27)}
-        
-        
-        dp = {}
+        d[""] = 1
         n = len(s)
-        
-        def helper(i):
-            if i == n:
-                return 1
-            if i >= n:
-                return 0 
-            if i not in dp:
-                
-                
-                dp[i] = sum([0]+[helper(j) for j in range(i+1,n+1) if s[i:j] in d])
-                
-                
-            return dp[i]
-        
-        return helper(0)
+        dp = [0 for i in range(n)] + [1]
+        for i in range(n-1,-1,-1):
+            for j in range(i+1,n+1):
+                if s[i:j] in d:
+                    dp[i]+=dp[j]
+                    
+        return dp[0]
