68bd6b41c3a9e5abe0b5649651e4351b48bcb4e8
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-10 20:25:15 -0500

    Time: 62 ms (14.93%), Space: 13.9 MB (85.60%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -3,10 +3,10 @@ class Solution:
         d = {str(i) : 1 for i in range(1,27)}
         d[""] = 1
         n = len(s)
-        dp = [0 for i in range(n)] + [1] + [0]
+        dp = [0 for i in range(n)] + [1]
         for i in range(n-1,-1,-1):
             for j in range(i+1,i+3):
                 if s[i:j] in d:
-                    dp[i]+=dp[j]
+                    dp[i]+=(dp[j] if j <= n else 0)
                     
         return dp[0]
