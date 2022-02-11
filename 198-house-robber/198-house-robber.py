05bec66fcbfe011b1bd60c5abe4badc6e32d71b5
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-10 19:16:16 -0500

    Time: 36 ms (61.97%), Space: 13.9 MB (93.23%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,17 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        dp = {}
-        
-        def helper(i):
-            if i >= len(nums):
-                return 0
+        dp = [ 0 for i in range(len(nums))]
+        n = len(nums)
+        for i in range(n-1,-1,-1):
             
+            dp[i] = max(nums[i] + (dp[i+2] if i + 2 < n else 0), (dp[i+1] if i + 1 < n else 0))
             
-            if i  not in dp:
-                
-                dp[i] = max( nums[i] + helper(i + 2), helper(i + 1))
-                
-            return dp[i]
-        
-        return helper(0)
+        return dp[0]
