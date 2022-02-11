2965fdfe96f70d54e01e26e63b6e8730792c052d
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-10 21:53:35 -0500

    Time: 61 ms (11.85%), Space: 14 MB (84.96%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,3 +1,22 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return math.factorial(m + n - 2)//math.factorial(m-1)//math.factorial(n-1)
+        d = defaultdict(int)
+        d = [[0 for i in range(n)] for j in range(m)]
+        d[0][0] = 1
+        q = [[0,0]]
+        v = {}
+        v[0,0] = 1
+        while q:
+            cx,cy = q.pop(0)
+            # print(cx,cy)
+            r = [cx,cy+1]
+            b = [cx+1,cy]
+            
+            for nx,ny in [r,b]:
+                if 0<=nx<m and 0<=ny<n:
+                    d[nx][ny]+=d[cx][cy]
+                    if (nx,ny) not in v:
+                        v[nx,ny] = 1
+                        q.append([nx,ny])
+        # print(d)
+        return d[m-1][n-1]
