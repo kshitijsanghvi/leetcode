0fc528f20d5b91cd067b6d090fed3786d5123a60
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-13 17:18:39 -0500

    Time: 348 ms (69.92%), Space: 23.6 MB (9.86%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,41 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n = len(grid),len(grid[0])
-
-        dp = [[i + j*n for i in range(n)] for j in range(m)]
-        
-        def find(x,y):
-            if dp[x][y] == x*n + y:
-                return x*n + y
-            else:
-                dp[x][y] = find(dp[x][y]//n,dp[x][y]-dp[x][y]//n*n)
+        v = {}
+        ans = 0
+        def bfs(cx,cy):
             
-            return dp[x][y]
-        
-        def union(p1,p2):
-            c1 = find(p1[0],p1[1])
-            c2 = find(p2[0],p2[1])
-            
-            dp[c1//n][c1-c1//n*n] = c2
-            
+            q = [[cx,cy]]
+            while q:
+                cx,cy = q.pop()
+                l = [cx,cy-1]
+                r = [cx,cy+1]
+                t = [cx-1,cy]
+                b = [cx+1,cy]
+                
+                for nx,ny in [l,r,t,b]:
+                    if 0 <= nx < m and 0 <=ny<n and (nx,ny) not in v and grid[nx][ny]=='1':
+                        v[nx,ny] = 1
+                        q.append([nx,ny])
+                        
         
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1':
-                    l = (i,j-1)
-                    r = (i,j+1)
-                    t = (i-1,j)
-                    b = (i+1,j)
-                    
-                    for nx,ny in [l,r,t,b]:
-                        if 0 <= nx < m and 0 <= ny<n and grid[nx][ny]=='1':
-                            if find(nx,ny) != find(i,j):
-                                union([i,j],[nx,ny])
-                                
-        d = {}
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1':
-                    d[find(i,j)] = 1
-        # print(dp)
-        return len(d)
+                if grid[i][j]== '1' and (i,j) not in v:
+                    v[i,j] = 1
+                    ans+=1
+                    bfs(i,j)
+        return ans
