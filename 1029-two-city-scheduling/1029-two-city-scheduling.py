3a05dc16b4e553745b1a7d2569e49f48327f5f0d
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-14 17:06:10 -0500

    Time: 62 ms (39.47%), Space: 14 MB (73.18%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,15 +1,12 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        dp = {}
-        def helper(i,n1,n2):
-            if i  == len(costs):
-                if n1 != n2:
-                    return math.inf
-                else:
-                    return 0
-                
-            if (i,n1,n2) not in dp:
-                dp[i,n1,n2] = min(costs[i][0] + helper(i+1,n1-1,n2),costs[i][1] + helper(i+1,n1,n2-1))
-            return dp[i,n1,n2]
+        costs_diff = [[i[0]-i[1],idx] for idx,i in enumerate(costs)]
+        costs_diff.sort(reverse=True)
+        ans = 0
+        for i in range(len(costs)//2):
+            ans+=costs[costs_diff[i][1]][1]
         
-        return helper(0,len(costs)//2,len(costs)//2)
+        for i in range(len(costs)//2,len(costs)):
+            ans+=costs[costs_diff[i][1]][0]
+        
+        return ans
