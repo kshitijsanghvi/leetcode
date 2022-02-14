9b8e86f5230bdcb90a2904fd91cbac5056fff645
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-14 00:16:55 -0500

    Time: 55 ms (46.94%), Space: 14 MB (85.05%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -2,7 +2,7 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         n = len(s)
         dp = [False for i in range(n)] + [True]
-        
+        wordDict = Counter(wordDict)
         for i in range(n-1,-1,-1):
             dp[i] = max([False]+[dp[j] for j in range(i+1,n+1) if s[i:j] in wordDict])
         return dp[0]
