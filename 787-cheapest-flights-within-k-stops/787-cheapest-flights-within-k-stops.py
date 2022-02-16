354cb77456497fa6e3d1a6d9da8056edf8c126cb
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-15 23:47:09 -0500

    Time: 6396 ms (5.00%), Space: 56.4 MB (5.52%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,35 +1,22 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        # K stops -> path length : k + 1
-        # Create a min heap based on price
-        # Prune node that have reached the stop limit
-        # Heapify on sum of cost
-        
-        h = [[0,-1,src]]
-        v = {}
-        v[src] = [0,-1]
-        d = defaultdict(list)
-
-        for i in flights:
-            d[i[0]].append([i[1],i[2]])
-        
-        while h:
-            cp,cs,cn = heapq.heappop(h)
-            if cn == dst:
-                return cp
-            # check dest before
-            if cs >= k:
-                continue
-                
-
+        adj = defaultdict(list)
+        for frm,to,price in flights:
+            adj[frm].append([to,price])
             
+        v = {}
+        h = [[0,src,0]]
+        v[src] = [0,0]
+        ans = []
+        while h:
+            cprice, cn, cstops = heapq.heappop(h)
+            if cn == dst:
+                 ans.append(cprice)
             else:
-                for nn,p in d[cn]:
-                    if nn not in v or v[nn][0] > cp + p or v[nn][1] > cs+1:
-
-                        v[nn] = [cp + p,cs+1]
-                        heapq.heappush(h,[cp+p,cs+1,nn])
-
-        return -1
-                
-                
+                nstops = cstops + 1
+                if nstops <= k + 1:
+                    for nn,nprice in adj[cn]:
+                        if nn not in v or v[nn][0] > cprice + nprice or v[nn][1] > nstops:
+                            v[nn] = [cprice + nprice,nstops]
+                            heapq.heappush(h,[cprice + nprice, nn, nstops])
+        return -1 if not ans else min(ans)
