e3c2821490790b6efafe504b18826d8919d2bf7c
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-20 15:35:00 -0500

    Time: 157 ms (60.95%), Space: 14.8 MB (83.57%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -2,9 +2,16 @@ class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
+        Runtime = O(mn)
+        Space = O(1)
+        
         Approach:
-        1. Mark inner matrix
-        2. Deal with the 4 corner cases
+        1. As we cannot use extra space the most straightforward approach is reusing available space 
+        2. The underpinning of the same is that we need to extract the information from the reusing memory space first before overwriting the space
+        3. The overwriting here is to differentiate if the row0 or col0 has a 0 before
+        4. We use the row0 and col0 to flag if that row and col needs to be assigned Zero
+        5. Thus we keep 
+        
         """
         m, n = len(matrix), len(matrix[0])
         
@@ -14,24 +21,24 @@ class Solution:
             if matrix[0][j] == 0:
                 row0 = True
                 break
-        rowM = False
+        # rowM = False
         
-        for j in range(n):
-            if matrix[m-1][j] == 0:
-                rowM = True
-                break
+        # for j in range(n):
+        #     if matrix[m-1][j] == 0:
+        #         rowM = True
+        #         break
         col0 = False
         
         for i in range(m):
             if matrix[i][0] == 0:
                 col0 = True
                 break
-        colN = False
+#         colN = False
         
-        for i in range(m):
-            if matrix[i][n-1] == 0:
-                colN = True
-                break
+#         for i in range(m):
+#             if matrix[i][n-1] == 0:
+#                 colN = True
+#                 break
         
         for i in range(1,m):
             for j in range(1,n):
@@ -50,16 +57,16 @@ class Solution:
         if row0:
             for j in range(n):
                 matrix[0][j] = 0
-        if rowM:
-            for j in range(n):
-                matrix[m-1][j] = 0
+        # if rowM:
+        #     for j in range(n):
+        #         matrix[m-1][j] = 0
                 
         if col0:
             for i in range(m):
                 matrix[i][0] = 0
-        if colN:
-            for i in range(m):
-                matrix[i][n-1] = 0
+        # if colN:
+        #     for i in range(m):
+        #         matrix[i][n-1] = 0
             
         
         
