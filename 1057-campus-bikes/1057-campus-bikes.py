357cc91ef06e9a4ba1d27e02d8a4007c45404838
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-19 19:35:57 -0500

    Time: 1171 ms (58.76%), Space: 110.2 MB (83.76%) - LeetHub

---
+++ 1057-campus-bikes/1057-campus-bikes.py
@@ -0,0 +1,34 @@
+class Solution:
+    def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> List[int]:
+        
+        def find_distance(worker_loc, bike_loc):
+            return abs(worker_loc[0] - bike_loc[0]) + abs(worker_loc[1] - bike_loc[1])
+        
+        min_dist = float('inf')
+        dist_to_pairs = collections.defaultdict(list)
+        
+        for worker, worker_loc in enumerate(workers):
+            for bike, bike_loc in enumerate(bikes):
+                distance = find_distance(worker_loc, bike_loc)
+                dist_to_pairs[distance].append((worker, bike))
+                min_dist = min(min_dist, distance)
+                
+        curr_dist = min_dist
+        # Initialize all values to false, to signify no bikes have been taken
+        bike_status = [False] * len(bikes)
+        # Initialize all values to -1, to signify no worker has a bike
+        worker_status = [-1] * len(workers)
+        # Keep track of how many worker-bike pairs have been made
+        pair_count = 0
+        
+        # While all workers have not been assigned a bike
+        while pair_count < len(workers):
+            for worker, bike in dist_to_pairs[curr_dist]:
+                if worker_status[worker] == -1 and not bike_status[bike]:
+                    # If both worker and bike are free, assign them to each other
+                    bike_status[bike] = True
+                    worker_status[worker] = bike
+                    pair_count += 1
+            curr_dist += 1
+        
+        return worker_status
