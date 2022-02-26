6a3631cc0234f9eeb630dc29308d5cd33372a48e
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-26 14:42:41 -0500

    Time: 615 ms (80.22%), Space: 28.9 MB (51.17%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -3,15 +3,22 @@ class Solution:
         n = len(startTime)
         startTime = [[startTime[i],i] for i in range(n)]
         startTime.sort()
-        dp = {}
-        def helper(i):
-            if i >= n:
-                return 0
+        dp = [0 for i in range(n+1)]
+#         def helper(i):
+#             if i >= n:
+#                 return 0
             
-            if i not in dp:
-                idx = startTime[i][1]
-                dp[i] = max(profit[idx] + helper(bisect.bisect_left(startTime,[endTime[idx],],lo = i + 1)), helper(i+1))
+#             if i not in dp:
+#                 idx = startTime[i][1]
+#                 dp[i] = max(profit[idx] + helper(bisect.bisect_left(startTime,[endTime[idx],],lo = i + 1)), helper(i+1))
             
-            return dp[i]
+#             return dp[i]
     
-        return helper(0)
+#         return helper(0)
+
+        for i in range(n-1,-1,-1):
+            dp[i] = max(profit[startTime[i][1]] + dp[bisect.bisect_left(startTime, [endTime[startTime[i][1]],],lo = i + 1)], dp[i+1])
+        
+        return dp[0]
+
+        
