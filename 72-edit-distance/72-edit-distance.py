3923eafaafb2950d721ee090d62052160abee3f0
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-27 17:39:36 -0500

    Time: 540 ms (5.03%), Space: 46.5 MB (5.15%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -7,13 +7,14 @@ class Solution:
         """
         Word1 tp Word2
         """
-        @lru_cache(None)
+        dp = {}
         def helper(i,j):
             if i == m:
                 return n - j
             if j == n:
                 return m - i
             # Insert Delete Substitute
-            return min(1 + helper(i,j+1),1 + helper(i+1,j),helper(i+1,j+1)+int(word1[i]!=word2[j]))
-                
+            if (i,j) not in dp:
+                dp[i,j]=min(1 + helper(i,j+1),1 + helper(i+1,j),helper(i+1,j+1)+int(word1[i]!=word2[j]))
+            return dp[i,j]
         return helper(0,0)
