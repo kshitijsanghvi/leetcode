95b9a0b04eb97e10f014828a9f0a76e453609337
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-27 17:50:23 -0500

    Time: 376 ms (9.14%), Space: 17.9 MB (19.14%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -7,14 +7,27 @@ class Solution:
         """
         Word1 tp Word2
         """
-        dp = {}
-        def helper(i,j):
-            if i == m:
-                return n - j
-            if j == n:
-                return m - i
-            # Insert Delete Substitute
-            if (i,j) not in dp:
-                dp[i,j]=min(1 + helper(i,j+1),1 + helper(i+1,j),helper(i+1,j+1)+int(word1[i]!=word2[j]))
-            return dp[i,j]
-        return helper(0,0)
+        # dp = {}
+        # def helper(i,j):
+        #     if i == m:
+        #         return n - j
+        #     if j == n:
+        #         return m - i
+        #     # Insert Delete Substitute
+        #     if (i,j) not in dp:
+        #         dp[i,j]=min(1 + helper(i,j+1),1 + helper(i+1,j),helper(i+1,j+1)+int(word1[i]!=word2[j]))
+        #     return dp[i,j]
+        # return helper(0,0)
+    
+    
+        dp = [[0 for i in range(n+1)] for i in range(m+1)]
+        for j in range(n+1):
+            dp[m][j] = n - j
+        for i in range(m + 1):
+            dp[i][n] = m - i
+            
+        for j in range(n-1,-1,-1):
+            for i in range(m - 1, -1, - 1):
+                dp[i][j] = min(1 + dp[i][j+1], 1 + dp[i+1][j], dp[i+1][j+1]+int(word1[i]!=word2[j]))
+                
+        return dp[0][0]
