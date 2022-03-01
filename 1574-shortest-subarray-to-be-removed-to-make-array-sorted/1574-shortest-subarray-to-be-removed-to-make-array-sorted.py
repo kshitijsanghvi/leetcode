d971f4f629e99fe24339650944bf384db4ce1352
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-02-28 20:58:36 -0500

    Time: 656 ms (82.43%), Space: 28.6 MB (86.61%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,22 +1,22 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
-        """
-        Approach : 
-        Trim
-        Binary search, take max
-        """
         nums = arr
         n = len(arr)
+        
         l = 1
         while l < n and nums[l] >= nums[l-1]:
             l +=1
+        
         if l == n:
             return 0
+        
         r = n - 2
         while r >= 0 and nums[r] <= nums[r+1]:
             r -= 1
+        
         l = l - 1
         r = r + 1
+        
         ans = min(r,n - l - 1)
         while l >= 0:
             if nums[l] <= nums[r]:
@@ -24,4 +24,5 @@ class Solution:
             idx = bisect.bisect_left(nums, nums[l],lo = r)
             ans = min(ans,idx - l - 1)
             l-=1
+ 
         return ans
