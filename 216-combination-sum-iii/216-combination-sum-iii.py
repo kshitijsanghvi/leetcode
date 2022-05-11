650e9ae222cb7757ac5c38f9cae579f154748e9f
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-05-10 21:24:03 -0400

    Time: 42 ms (57.23%), Space: 14 MB (29.59%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.py
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -1,29 +1,22 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        ans = set()
-        def helper(n,k,contains):
-            nonlocal ans
-            # base case 
-            if n == 0 and k == 0:
-                # print(bin(contains))
-                
-                current_ans = []
-                for i in range(9):
-                    ele = 1 << i
-                    if ele & contains != 0:
-                        current_ans.append(i+1)
-                current_ans.sort()
-                ans.add(tuple(current_ans))
-            
-            elif k == 0 or n < 0:
+        seen = [0] * 10
+        current_ans = [0]
+        ans = []
+        def helper(current_sum, current_count):
+            if current_count == k:
+                if current_sum == n:
+                    ans.append(current_ans[1:])
                 return
-            
-            #recurrence relation 
-            for i in range(9):
-                ele = 1 << i
-                if contains & ele == 0:
-                    # print(bin(contains), bin(contains | ele) )
-                    helper(n - i-1, k - 1,contains | ele)
-                    
-        helper(n,k,0)
+            else:
+                
+                for i in range(current_ans[-1]+1,10):
+                    if not seen[i]:
+                        seen[i] = 1
+                        current_ans.append(i)
+                        helper(current_sum + i, current_count + 1)
+                        seen[i] = 0
+                        current_ans.pop()
+
+        helper(0,0)
         return ans
