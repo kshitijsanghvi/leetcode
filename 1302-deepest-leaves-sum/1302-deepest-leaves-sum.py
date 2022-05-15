4c44a392ca4c274b174ca53bf33b866838af5e76
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-05-15 15:52:21 -0400

    Time: 402 ms (14.81%), Space: 17.9 MB (28.26%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        """
+        Create a dictionary and sum all elements at equal depth
+        find max key and return 
+        """
+        
+        if not root:
+            return None
+        
+        else:
+            
+            d = Counter()
+            
+            def helper(node,depth):
+                if node:
+                    d[depth] += node.val
+                    helper(node.left,depth+1)
+                    helper(node.right,depth + 1)
+            # I forget to call the helper function before returning
+            helper(root,0)
+            return d[max(d.keys())]
+                    
