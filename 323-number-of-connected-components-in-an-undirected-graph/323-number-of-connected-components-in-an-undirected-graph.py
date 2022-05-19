5487a8f001eb0d1d32ac8a59874fecda2b12acd1
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-05-18 20:49:50 -0400

    Time: 127 ms (57.46%), Space: 15.5 MB (50.79%) - LeetHub

---
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = collections.defaultdict(list)
+        
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            
+        color = [0] * n
+        
+        def bfs(i):
+            nonlocal color
+            q = deque()
+            q.append(i)
+            color[i] = 1
+            while q:
+                cn = q.popleft()
+                for nn in adj[cn]:
+                    if color[nn] == 0:
+                        q.append(nn)
+                color[cn] = 2
+        ans = 0
+        for i in range(n):
+            if color[i] == 0:
+                ans += 1
+                bfs(i)
+                
+        return ans
