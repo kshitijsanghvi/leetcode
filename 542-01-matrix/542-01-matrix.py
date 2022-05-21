378ac72fb1d1455e3ff671b09b2de938cab23e29
kshitijsanghvi <kshitijsanghvi@yahoo.com>
2022-05-21 13:48:28 -0400

    Time: 641 ms (89.86%), Space: 16.8 MB (90.77%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -2,24 +2,25 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         q = deque()
         m, n = len(mat), len(mat[0])
-        color = defaultdict(int)
+        
+        
         for i in range(m):
             for j in range(n):
-                if mat[i][j] == 0:
-                    q.append([i,j,0])
+                if mat[i][j] == 1:
+                    mat[i][j] = math.inf
+                if i - 1 >= 0:
+                    mat[i][j] = min(mat[i][j], 1 + mat[i-1][j])
+                
+                if j -1 >= 0:
+                    mat[i][j] = min(mat[i][j], 1 + mat[i][j-1])
         
-        while q:
-            ci,cj,cd = q.popleft()
-            l = [ci,cj-1]
-            r = [ci,cj + 1]
-            t = [ci-1,cj]
-            b = [ci+1,cj]
-            
-            for ni,nj in [l,r,t,b]:
-                if 0 <= ni <m and 0<=nj<n:
-                    if mat[ni][nj]!= 0 and color[ni,nj] == 0:
-                        color[ni,nj] = 1
-                        mat[ni][nj] = cd + 1
-                        q.append([ni,nj,cd+1])
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                
+                if i + 1 < m:
+                    mat[i][j] = min(mat[i][j], 1 + mat[i+1][j])
+                    
+                if j+ 1 < n:
+                    mat[i][j] = min(mat[i][j], 1 +  mat[i][j + 1])
+                    
         return mat
-        
